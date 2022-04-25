273d0b6e90329f407ad8705acc2bfde5e0ef47fe
Owillz <khodionwill@gmail.com>
2022-04-25 14:35:27 +0100

    added observer pattern

---
+++ src/main/java/uk/ac/tees/scedt/b1349506/Observed.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uk.ac.tees.scedt.b1349506;
+
+/**
+ *
+ * @author godsw
+ */
+interface Observed {
+    public void registerObserver(Observer o);
+    public void updateObserver();
+}
