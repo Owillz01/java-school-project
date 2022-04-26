e3b4d052e7565d1a20b77778e152357ad4fec180
Owillz <khodionwill@gmail.com>
2022-04-26 16:52:48 +0100

    added sqlite class

---
+++ src/main/java/uk/ac/tees/scedt/b1349506/Sqlite.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uk.ac.tees.scedt.b1349506;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author godsw
+ */
+public class Sqlite {
+    private static Connection con;   
+    private static boolean hasData = false;
+    
+//    public ResultSet displayStockItems() {
+//        if(con == null) {
+//            getConnection();
+//        }
+//    }
+
+}
